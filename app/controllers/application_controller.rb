c4f71f8096753e264458168643aa8e158fb20f2b
vishwnath yadav <vyadav@focusvision.com>
2019-04-17 23:32:26 +0530

    add in new commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   #i have changedd
+
+  
   protect_from_forgery with: :exception
 end
